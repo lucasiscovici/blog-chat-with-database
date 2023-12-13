3d8b77d3ded6356e07d6d876a2b905a618ba2ec8
lucasiscovici-loreal <lucas.iscovici@rd.loreal.com>
2023-12-13 17:36:54 +0100

    add facts (prompt, llm, ...)

--- blog_langchain_db/chain.py
+++ blog_langchain_db/chain.py
@@ -3,16 +3,14 @@ from langchain.chains.base import Chain
 from langchain.utilities import SQLDatabase
 from langchain.chains import LLMChain
 
-from .prompts import (
-    SQL_QUERY_PROMPT,
-    RESPONSE_PROMPT
-)
+from .prompts import SQL_QUERY_PROMPT, RESPONSE_PROMPT, FACTS_PROMPT
 
 class SQLChain(Chain):
 
     db: SQLDatabase
     llm_chain_query: LLMChain
     llm_chain_response: LLMChain
+    llm_chain_facts: LLMChain
     table_descriptions: str
 
     input_key: str = "question" #: :meta private:
@@ -26,26 +24,40 @@ class SQLChain(Chain):
     def output_keys(self):
         return [self.output_key]
 
-    def _call(
-        self,
-        inputs,
-        run_manager = None
-    ):
+    def _call(self, inputs, run_manager=None):
         # get table names
-        table_names_list = self.db.get_usable_table_names() 
+        table_names_list = self.db.get_usable_table_names()
 
         # get table infos
         table_infos_str = self.db.get_table_info_no_throw(table_names_list)
 
+        # get facts
+        facts = self.llm_chain_facts.run(
+            {
+                **inputs,
+                "schemas": table_infos_str,
+                "descriptions": self.table_descriptions,
+            }
+        )
+
         # generate the sql query
-        sql_query = self.llm_chain_query.run({**inputs, "schemas": table_infos_str, "descriptions": self.table_descriptions})
+        sql_query = self.llm_chain_query.run(
+            {
+                **inputs,
+                "schemas": table_infos_str,
+                "descriptions": self.table_descriptions,
+                "facts": facts,
+            }
+        )
         print("sql_query", sql_query)
 
         # execute the sql query
         query_response = self.db.run_no_throw(sql_query)
-        
+
         # generate the response
-        response = self.llm_chain_response.run({**inputs, "response": query_response})
+        response = self.llm_chain_response.run(
+            {**inputs, "query": sql_query, "facts": facts, "response": query_response}
+        )
 
         return {self.output_key: response}
 
@@ -57,6 +69,7 @@ class SQLChain(Chain):
         llm,
         prompt_sql_query = SQL_QUERY_PROMPT,
         prompt_response = RESPONSE_PROMPT,
+        prompt_facts = FACTS_PROMPT,
         table_descriptions="",
         verbose = False,
         **kwargs
@@ -66,6 +79,7 @@ class SQLChain(Chain):
             llm=llm, 
             llm_chain_query=LLMChain(llm=llm, prompt=prompt_sql_query, verbose=verbose), 
             llm_chain_response=LLMChain(llm=llm, prompt=prompt_response, verbose=verbose),
+            llm_chain_facts=LLMChain(llm=llm, prompt=prompt_facts, verbose=verbose),
             table_descriptions=table_descriptions,
             **kwargs
         )
--- blog_langchain_db/prompts.py
+++ blog_langchain_db/prompts.py
@@ -1,11 +1,31 @@
 from langchain.prompts import PromptTemplate
 
+
+FACTS_PROMPT_STR = """
+-- Here is the question of the user :
+{question}
+-- Here are the descriptions of the table schemas; please read them carefully to better understand the table:
+{descriptions}
+-- Here are the table schemas :
+{schemas}
+Review the tables, noting that some columns are for filtering ("Key Columns" or "Index Columns") and others contain specific values ("Data Columns" or "Value Columns").
+-- Create a bulleted list identifying all implicit facts from a user’s question, focusing on implicit values in the 'Key' or 'Index Columns.'
+Use common sense to infer only obvious values, include pertinent date information (months/years, ...), and especially specify all necessary calculations (e.g., using SQL SUM, MAX, or simple column selection).
+Don't make hasty decisions; try to be factual and infer values only when truly necessary, using common sense.
+Ensure your decisions are thoughtful and factual, and then, rephrase the user’s question based on these facts, indicating it's a potential rewrite.
+Present your answer as a bulleted list:
+- """
+FACTS_PROMPT = PromptTemplate.from_template(FACTS_PROMPT_STR)
+
+
 SQL_QUERY_PROMPT_TEXT="""-- Language : SQL
 -- Dialect : SQLITE
 -- Here are the descriptions of the table schemas; please read them carefully to better understand the table:
 {descriptions}
 -- Here are the table schemas :
 {schemas}
+-- Here are the interesting facts:
+{facts}
 -- A SQL query to return 1 and a SQL query for "{question}". Please only make a stand-alone query. DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
 SELECT 1;"""
 
@@ -14,6 +34,8 @@ SQL_QUERY_PROMPT = PromptTemplate.from_template(SQL_QUERY_PROMPT_TEXT)
 RESPONSE_PROMPT_TEXT="""As a SQL specialist, I'll format the answer to your question and query response, ensuring a conversational, polite, formal, and factual presentation, while adhering to standard financial notation for numerical values.
 
 The user's question : {question}.
+The facts: {facts}.
+The SQL query: {query}.
 The query response : {response}.
 The answer:"""
 
